e5a6a9526038a73543d49734fd113220cc1e0f59
HamzaAitFeska <77283740+HamzaAitFeska@users.noreply.github.com>
2022-10-06 18:45:33 +0200

    Slight Changes

--- Assets/Scripts/PlayerController/MainMenu.cs
+++ Assets/Scripts/PlayerController/MainMenu.cs
@@ -6,6 +6,7 @@ public class MainMenu : MonoBehaviour
     private void Start()
     {
         GameController.DestroySingleton();
+        Cursor.lockState = CursorLockMode.None;
     }
     public void OnstartClicked()
     {
